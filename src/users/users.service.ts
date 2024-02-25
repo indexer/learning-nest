f124664f1b5740ccf33889fe2c8a4390e95c9cf0
indexer <yemonkyaw007@gmail.com>
2024-02-25 13:18:51 +0800

    update callback in success

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,18 +2,18 @@ import { Injectable, Inject } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity/user.entity';
+import { get } from 'http';
 
 @Injectable()
-export class UsersService {
+export class UsersService { 
   constructor(@InjectRepository(User) private usersRepository: Repository<User>) { }
   
-  async getUsers(user: User): Promise<User[]> {
+  async getUsers(): Promise<User[]> {
     return await this.usersRepository.find();
   }
 
   async updateUser(user: User) : Promise<User[]> {
-    this.usersRepository.save(user)
-    return this.usersRepository.find()
+    return await this.usersRepository.save(user).then(() => this.getUsers(), () => [])
   }
   
   
